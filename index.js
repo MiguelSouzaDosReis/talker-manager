e2f9a6e239fdc9ab24dac0482eb098283b4ec515
Miguel_Reis <guelsouzar@gmail.com>
2022-02-23 06:20:56 -0300

    requisito 1

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+// no requisito 1, utilizei o repositorio do ANTONIO CAMPOS como base para o meu app.get('/talker')
+// https://github.com/tryber/sd-015-b-project-talker-manager/pull/115/commits/a02dbab4f11f432056647186534645948e5fc87e
+app.get('/talker', (_req, res) => {
+  fs.readFile('talker.json').then((data) => {
+    if (data.length > 0) { return res.status(200).json(JSON.parse(data)); }
+    return res.status(200).json([]);
+  });
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
